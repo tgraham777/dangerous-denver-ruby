52bcd2d1dc2013ad46ab207e7f5f2e2fa2c03a32
Tyler Graham <tylergraham134@gmail.com>
2016-01-06 20:48:14 -0700

    finishes neighborhood

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,13 +8,10 @@ class Parser
 
   def parse
     neighborhood = []
-    address = []
-    CSV.foreach(@filename,:headers => true, :header_converters => :symbol, :converters => :numeric) do |row|
+    CSV.foreach(@filename,:headers => true, :header_converters => :symbol) do |row|
       neighborhood << row[:neighborhood_id]
-      address << row[:incident_address]
     end
 
     Sorter.find_sketchiest_locations(neighborhood.compact)
-    Sorter.find_sketchiest_locations(address.compact)
   end
 end
